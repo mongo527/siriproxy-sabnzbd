b0d48d19291da634989873da36b9cd9ee54aeab5
Shawn Conroyd <mongo527@gmail.com>
2012-02-27 21:08:23 -0500

    Testing a way for Siri to say Sabnzbd correctly

--- lib/siriproxy-sabnzbd.rb
+++ lib/siriproxy-sabnzbd.rb
@@ -21,14 +21,16 @@ class SiriProxy::Plugin::Sabnzbd < SiriProxy::Plugin
     	begin
     		sab = sabParser("pause")
     		if sab["status"]
-    			say "Sabnzbd has paused all downloads", spoken: "SabNZBD has paused all downloads"
+    			say "Sabnzbd has paused all downloads", 
+    			spoken: "SabN-Z-B-D has paused all downloads"
     		elsif not sab["status"]
     			if sab["error"].downcase == "api key incorrect"
     				say "Sorry, the API Key is incorrect"
     			elsif sab["error"].downcase == "api key required"
     				say "Sorry, API Key was not given in the config file"
     			else
-    				say "Sorry, I could not pause Sab"
+    				say "Sorry, I could not pause Sabnzbd", 
+    				spoken: "Sorry, I could not pause SabNZBD"
     			end
     		end
     	rescue Errno::EHOSTUNREACH
@@ -47,14 +49,16 @@ class SiriProxy::Plugin::Sabnzbd < SiriProxy::Plugin
     	begin
     		sab = sabParser("resume")
     		if sab["status"]
-    			say "Sabnzbd has resumed all downloads"
+    			say "Sabnzbd has resumed all downloads", 
+    			spoken: "Sab NZBD has resumed all downloads"
     		elsif not sab["status"]
     			if sab["error"].downcase == "api key incorrect"
     				say "Sorry, the API Key is incorrect"
     			elsif sab["error"].downcase == "api key required"
     				say "Sorry, API Key was not given in the config file"
     			else
-    				say "Sorry, I could not resume Sab"
+    				say "Sorry, I could not resume Sabnzbd",
+    				spoken: "Sorry, I could not resume SabNZBD"
     			end
     		end
     	rescue Errno::EHOSTUNREACH

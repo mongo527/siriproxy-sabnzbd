e97d792ad5dbc114f43cd3e7f1c32718b23ce783
Shawn Conroyd <mongo527@gmail.com>
2012-02-28 19:55:04 -0500

    Added pause and resume individual nzbs

--- lib/siriproxy-sabnzbd.rb
+++ lib/siriproxy-sabnzbd.rb
@@ -73,6 +73,56 @@ class SiriProxy::Plugin::Sabnzbd < SiriProxy::Plugin
         request_completed
     end
     
+    listen_for /pause (.+)/i do |response|
+    	begin
+			queue = sabParser("queue")
+			nzb = queue["queue"]["slots"]
+			if not nzb.empty?
+				for i in nzb
+					if /(.*)(#{response})(.*)/.match(i["filename"].downcase)
+						nzbName = i["filename"]
+						nzbValue = i["nzo_id"]
+						success = true;
+						break;
+					else
+						success = false;
+					end
+				end
+			elsif nzb.empty?
+				say "There is nothing to pause"
+			else
+				if queue["error"].downcase == "api key incorrect"
+					say "Sorry, the API Key is incorrect"
+				elsif queue["error"].downcase == "api key required"
+					say "Sorry, API Key was not given in the config file"
+				end
+			end
+					
+			if success
+				sab = sabParser("queue&name=pause&value=#{nzbValue}")
+				if sab["status"]
+					say "#{nzbName} has been paused",
+					spoken: "#{response} has been paused"
+				else
+					say "Sorry, #{nzbName} could not be paused",
+					spoken: "Sorry, #{response} could not be paused"
+				end
+			else
+				say "Sorry, I could not find #{response} in the queue"
+			end
+    	rescue Errno::EHOSTUNREACH
+    		say "Sorry, I could not connect to Sabnzbd",
+    		spoken: "Sorry, I could not connect to Sab NZBD"
+    	rescue Errno::ECONNREFUSED
+    		say "Sorry, Sabnzbd is not currently running",
+    		spoken: "Sorry, Sab NZBD is not currently running"
+    	rescue Errno::ENETUNREACH
+            say "Sorry, Could not connect to the network"
+        rescue Errno::ETIMEDOUT
+            say "Sorry, The operation timed out"
+		end
+	end
+    
     listen_for /resume (my downloads|saab|sab)/i do
     	begin
     		sab = sabParser("resume")
@@ -103,29 +153,79 @@ class SiriProxy::Plugin::Sabnzbd < SiriProxy::Plugin
         request_completed
     end
     
+    listen_for /resume (.+)/i do |response|
+    	begin
+			queue = sabParser("queue")
+			nzb = queue["queue"]["slots"]
+			if not nzb.empty?
+				for i in nzb
+					if /(.*)(#{response})(.*)/.match(i["filename"].downcase) AND i["status"].downcase == "paused"
+						nzb = i["filename"]
+						nzbValue = i["nzo_id"]
+						success = true;
+						break;
+					else
+						success = false;
+					end
+				end
+			elsif nzb.empty?
+				say "There is nothing to resume"
+			else
+				if queue["error"].downcase == "api key incorrect"
+					say "Sorry, the API Key is incorrect"
+				elsif queue["error"].downcase == "api key required"
+					say "Sorry, API Key was not given in the config file"
+				end
+			end
+					
+			if success
+				sab = sabParser("queue&name=resume&value=#{nzbValue}")
+				if sab["status"]
+					say "#{nzb} has been resumed",
+					spoken: "#{response} has been resumed"
+				else
+					say "Sorry, #{nzb} could not be resumed",
+					spoken: "Sorry, #{response} could not be resumed"
+				end
+			else
+				say "Sorry, I could not find #{response} in the queue"
+			end
+    	rescue Errno::EHOSTUNREACH
+    		say "Sorry, I could not connect to Sabnzbd",
+    		spoken: "Sorry, I could not connect to Sab NZBD"
+    	rescue Errno::ECONNREFUSED
+    		say "Sorry, Sabnzbd is not currently running",
+    		spoken: "Sorry, Sab NZBD is not currently running"
+    	rescue Errno::ENETUNREACH
+            say "Sorry, Could not connect to the network"
+        rescue Errno::ETIMEDOUT
+            say "Sorry, The operation timed out"
+		end
+	end
+    
     listen_for /(what is|whats) downloading/i do
     	
     	begin
-    		nzb = getQueue()
-    		say nzb.class
-    		#if nzb.kind_of? Array
-			#	for i in nzb
-			#		say "#{i['filename']} is #{i['percentage']}% done", 
-			#		spoken: ""
-			#	end
-			#	say "The queue will be complete in #{sab['queue']['timeleft']}"
-			#elsif nzb.empty?
-			#	say "Nothing is currently downloading"
-			#else
-			#	if sab["error"].downcase == "api key incorrect"
-    		#		say "Sorry, the API Key is incorrect"
-    		#	elsif sab["error"].downcase == "api key required"
-    		#		say "Sorry, API Key was not given in the config file"
-    		#	else
-    		#		say "Sorry, I could not resume Sabnzbd",
-    		#		spoken: "Sorry, I could not resume Sab NZBD"
-    		#	end
-			#end
+    		sab = sabParser("queue")
+			nzb = sab["queue"]["slots"]
+    		if not nzb.empty?
+				for i in nzb
+					say "#{i['filename']} is #{i['percentage']}% done", 
+					spoken: ""
+				end
+				say "The queue will be complete in #{sab['queue']['timeleft']}"
+			elsif nzb.empty?
+				say "Nothing is currently downloading"
+			else
+				if sab["error"].downcase == "api key incorrect"
+    				say "Sorry, the API Key is incorrect"
+    			elsif sab["error"].downcase == "api key required"
+    				say "Sorry, API Key was not given in the config file"
+    			else
+    				say "Sorry, I could not resume Sabnzbd",
+    				spoken: "Sorry, I could not resume Sab NZBD"
+    			end
+			end
 		rescue Errno::EHOSTUNREACH
     		say "Sorry, I could not connect to Sabnzbd",
     		spoken: "Sorry, I could not connect to Sab NZBD"
@@ -138,27 +238,6 @@ class SiriProxy::Plugin::Sabnzbd < SiriProxy::Plugin
             say "Sorry, The operation timed out"
         end
         request_completed
-    end
-    
-    def getQueue()
-    	queue = Array.new
-    	begin
-			sab = sabParser("queue")
-			nzb = sab["queue"]["slots"]
-			
-			return nzb
-			
-		rescue Errno::EHOSTUNREACH
-    		return say "Sorry, I could not connect to Sabnzbd",
-    		spoken: "Sorry, I could not connect to Sab NZBD"
-    	rescue Errno::ECONNREFUSED
-    		return say "Sorry, Sabnzbd is not currently running",
-    		spoken: "Sorry, Sab NZBD is not currently running"
-    	rescue Errno::ENETUNREACH
-            return say "Sorry, Could not connect to the network"
-        rescue Errno::ETIMEDOUT
-            return say "Sorry, The operation timed out"
-        end
     end
     
     def sabParser(cmd)
